13c612693f003a49c602f1712da5f723c6ccdbcc
Guilhermevitor044 <162389771+Guilhermevitor044@users.noreply.github.com>
2026-06-16 20:53:42 -0300

    Criando o projeto do backend com o spring io

---
+++ backend/bolao-copa-api/src/main/java/br/com/bolao/copa/BolaoCopaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.bolao.copa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BolaoCopaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BolaoCopaApiApplication.class, args);
+	}
+
+}
