cb6c29e1c522477bf2e9bcf5ae5ac12f18e0fc29
Guilhermevitor044 <162389771+Guilhermevitor044@users.noreply.github.com>
2026-06-18 23:52:02 -0300

    Criando repository do usuario

--- backend/bolao-copa-api/src/main/java/br/com/bolao/copa/repository/UsuarioRepository.java
+++ backend/bolao-copa-api/src/main/java/br/com/bolao/copa/repository/UsuarioRepository.java
@@ -4,4 +4,8 @@ import br.com.bolao.copa.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Usuario findByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
