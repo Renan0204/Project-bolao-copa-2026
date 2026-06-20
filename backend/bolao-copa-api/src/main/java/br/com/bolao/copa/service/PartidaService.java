ff06d610139790f63d52a2ce07b3d8686a3a0466
Renan0204 <ro6680476@gmail.com>
2026-06-19 21:41:08 -0300

    Criando service de partida

---
+++ backend/bolao-copa-api/src/main/java/br/com/bolao/copa/service/PartidaService.java
@@ -0,0 +1,33 @@
+package br.com.bolao.copa.service;
+
+import br.com.bolao.copa.model.Partida;
+import br.com.bolao.copa.repository.PartidaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PartidaService {
+
+    private final PartidaRepository partidaRepository;
+
+    public PartidaService(PartidaRepository partidaRepository) {
+        this.partidaRepository = partidaRepository;
+    }
+
+    public List<Partida> listarTodas() {
+        return partidaRepository.findAll();
+    }
+
+    public void salvar(Partida partida) {
+        partidaRepository.save(partida);
+    }
+
+    public Partida buscarPorId(Long id) {
+        return partidaRepository.findById(id).orElse(null);
+    }
+
+    public void excluir(Long id) {
+        partidaRepository.deleteById(id);
+    }
+}
